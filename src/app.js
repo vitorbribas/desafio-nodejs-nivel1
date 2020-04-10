fdb231ed8054f0f25ca64391015b032d1378617e
Vítor <bsbribas66@gmail.com>
2020-04-10 18:36:32 -0300

    add repository update method

--- src/app.js
+++ src/app.js
@@ -32,7 +32,22 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.'});
+  }
+
+  repositories[repositoryIndex] = {
+    title,
+    url,
+    techs
+  };
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
